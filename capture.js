3c1068be8950568b947e9523d40378ad7ffaaf98
Yuji Nishimura <nishimura.yuji@liclab.com>
2018-02-27 00:12:52 +0900

    change log format & screenshot function

--- capture.js
+++ capture.js
@@ -4,12 +4,28 @@ const fs = require('fs');
 const puppeteer = require('puppeteer');
 const resemble = require('node-resemble-js');
 const Jimp = require('jimp');
+const sprintf = require('sprintf-js').sprintf;
 
 const config = JSON.parse(fs.readFileSync('./config.json'));
 
 resemble.outputSettings({
   transparency: 0.2
 });
+
+const ss = async (p, c, u, ss) => {
+    await p.goto(u);
+    await p.waitFor(500);
+    for (let i = 0; i < c.pre.length; i++) {
+        await p.evaluate((f) => {
+            eval(f);
+        }, c.pre[i]);
+        await p.waitFor(300);
+    }
+    await p.screenshot({
+        path: ss,
+        fullPage: true
+    });
+}
 
 (async () => {
     const browser = await puppeteer.launch();
@@ -18,44 +34,24 @@ resemble.outputSettings({
         await page.emulate(config.emulation);
     }
 
-    for (let i = 0; i < config.targets.length; i++) {
+    const targetNum = config.targets.length;
+    for (let i = 0; i < targetNum; i++) {
         const t = config.targets[i];
+        const u = t.url;
 
         const ssname = t.url ? t.url.replace(/\/$/, '').replace('/','___') + '.png' : 'index.png';
 
         const stableUrl = config.stable + t.url;
         const stableSsPath = `${config.dir}stable/${ssname}`;
-        await page.goto(`${stableUrl}`);
-        await page.waitFor(7000);
-        for (let j = 0; j < config.pre.length; j++) {
-            await page.evaluate((f) => {
-                eval(f);
-            }, config.pre[j]);
-            await page.waitFor(300);
-        }
-        await page.screenshot({
-            path: `${config.dir}stable/${ssname}`,
-            fullPage: true
-        });
+        await ss(page, config, stableUrl, stableSsPath);
 
         const upstreamUrl = config.upstream + t.url;
         const upstreamSsPath = `${config.dir}upstream/${ssname}`;
-        await page.goto(`${upstreamUrl}`);
-        await page.waitFor(7000);
-        for (let j = 0; j < config.pre.length; j++) {
-            await page.evaluate((f) => {
-                eval(f);
-            }, config.pre[j]);
-            await page.waitFor(300);
-        }
-        await page.screenshot({
-            path: `${config.dir}upstream/${ssname}`,
-            fullPage: true
-        });
+        await ss(page, config, upstreamUrl, upstreamSsPath);
 
         const diffSsPath = `${config.dir}diff/${ssname}`;
         resemble(stableSsPath).compareTo(upstreamSsPath).onComplete((data) => {
-            if(data.misMatchPercentage > 0) {
+            if (data.misMatchPercentage > 0) {
                 const stream = data.getDiffImage().pack();
                 let buffer = new Buffer([])
                 stream.on('data', (data) => {
@@ -68,10 +64,14 @@ resemble.outputSettings({
                         });
                     });
                 });
+    
+                console.log(sprintf("[%03d/%03d]\t%s\t%s", (i + 1), targetNum, 'ng', u));
             } else {
                 fs.copyFile('./banzai_people.png', diffSsPath, (err) => {});
+                console.log(sprintf("[%03d/%03d]\t%s\t%s", (i + 1), targetNum, 'ok', u));
             }
         });
+        await page.waitFor(1000);
     }
 
     await browser.close();
